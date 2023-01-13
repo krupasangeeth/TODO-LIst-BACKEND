c71ff74a81e86b1f716f2350bd742d1f56ff6991
krupa <krupasangeeth107@gmail.com>
2023-01-14 00:15:35 +0530

    Simple tasks to connect front end to back end

--- src/main/java/com/example/rest/webservice/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/rest/webservice/restfulwebservice/HelloWorldController.java
@@ -1,11 +1,14 @@
 package com.example.rest.webservice.restfulwebservice;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 // import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
 public class HelloWorldController {
 
     // @RequestMapping(method = RequestMethod.GET,path = "/helloworld") Or
@@ -16,6 +19,14 @@ public class HelloWorldController {
 
     @GetMapping(path = "/helloworldbean")
     public HelloWorldBean helloWorldBean(){
-        return new HelloWorldBean("Hii!!!! Gud Mrng");
+        throw new RuntimeException("Some Error Occured,Contact Support *******");
+        // return new HelloWorldBean("Hii!!!! Gud Night");
+    }
+
+    //path Variable
+    @GetMapping(path = "/hello/{name}")
+    public HelloWorldBean paraName(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello , %s", name));
+        
     }
 }
